5da155ea777bd7d8e353c70532085a8913ff9898
edytaroz <edytaroz11@poczta.fm>
2023-01-05 18:00:11 +0100

    SinglePushOut algorithm version 1 done

---
+++ SinglePushOut.py
@@ -0,0 +1,23 @@
+import networkx as nx
+
+
+def spo(left, right, main):
+
+    to_delete = []
+    for i in list(left.nodes):
+        main.remove_edges_from(left.edges(i))
+        if i not in list(right.nodes):
+            to_delete.append(i)
+    for i in to_delete:
+        main.remove_node(i)
+
+    new_nodes = []
+    for i in list(right.nodes):
+        if i not in list(left.nodes):
+            new_nodes.append(i)
+    for i in new_nodes:
+        main.add_node(i)
+    for i in list(right.nodes):
+        main.add_edges_from(right.edges(i))
+
+    return main
