9c7e9219e87789ccab2f5b681de1b6e858c04410
edytaroz <edytaroz11@poczta.fm>
2023-01-06 16:10:14 +0100

    SinglePushOut algorithm version 2 done

--- SinglePushOut.py
+++ SinglePushOut.py
@@ -1,23 +1,36 @@
-import networkx as nx
-
 
 def spo(left, right, main):
 
     to_delete = []
-    for i in list(left.nodes):
-        main.remove_edges_from(left.edges(i))
-        if i not in list(right.nodes):
-            to_delete.append(i)
+    for i, label in enumerate(left.labels):
+        if label not in right.labels:
+            to_delete.append(label)
     for i in to_delete:
-        main.remove_node(i)
+        for j in range(len(main.labels)):
+            if i == main.labels[j]:
+                main.G.remove_edges_from(list(main.G.edges(j)))
+                main.G.remove_node(j)
+                break
 
     new_nodes = []
-    for i in list(right.nodes):
-        if i not in list(left.nodes):
-            new_nodes.append(i)
-    for i in new_nodes:
-        main.add_node(i)
-    for i in list(right.nodes):
-        main.add_edges_from(right.edges(i))
+    for i, label in enumerate(right.labels):
+        if label not in left.labels:
+            new_nodes.append(label)
+    for i in range(len(new_nodes)):
+        main.G.add_node(len(main.labels)+i,label=new_nodes[i])
 
-    return main
+    for i in list(right.G.edges):
+        print(i)
+        a = right.labels[i[0]]
+        b = right.labels[i[1]]
+        a1 = 0
+        b1 = 0
+        for j in range(len(main.labels)):
+            if main.labels[j] == a:
+                a1 = j
+            if main.labels[j] == b:
+                b1 = j
+        main.G.add_edge(a1,b1)
+
+    return main.G
+
