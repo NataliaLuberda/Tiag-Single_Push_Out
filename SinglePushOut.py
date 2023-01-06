e7b4b35022b45a7547c9ec3dd833257bb566be34
edytaroz <edytaroz11@poczta.fm>
2023-01-06 20:46:13 +0100

    SinglePushOut algorithm version 3 done

--- SinglePushOut.py
+++ SinglePushOut.py
@@ -1,36 +1,43 @@
 
-def spo(left, right, main):
+def spo(left, right, m):
 
     to_delete = []
     for i, label in enumerate(left.labels):
         if label not in right.labels:
             to_delete.append(label)
     for i in to_delete:
-        for j in range(len(main.labels)):
-            if i == main.labels[j]:
-                main.G.remove_edges_from(list(main.G.edges(j)))
-                main.G.remove_node(j)
+        for j in range(len(m.labels)):
+            if i == m.labels[j]:
+                m.G.remove_edges_from(list(m.G.edges(j)))
+                m.G.remove_node(j)
                 break
 
     new_nodes = []
+    new_labels = []
     for i, label in enumerate(right.labels):
         if label not in left.labels:
             new_nodes.append(label)
     for i in range(len(new_nodes)):
-        main.G.add_node(len(main.labels)+i,label=new_nodes[i])
+        new_labels.append(new_nodes[i])
+        m.G.add_node(len(m.labels)+i,label=new_nodes[i])
+
+    m.labels = m.labels + new_labels
 
     for i in list(right.G.edges):
-        print(i)
         a = right.labels[i[0]]
         b = right.labels[i[1]]
-        a1 = 0
-        b1 = 0
-        for j in range(len(main.labels)):
-            if main.labels[j] == a:
-                a1 = j
-            if main.labels[j] == b:
-                b1 = j
-        main.G.add_edge(a1,b1)
 
-    return main.G
+        if a in m.labels and b in m.labels:
+            a1 = 0
+            b1 = 0
+            for j in range(len(m.labels)):
+                if m.labels[j] == a:
+                    a1 = j
+                if m.labels[j] == b:
+                    b1 = j
+            m.G.add_edge(a1,b1)
+
+
+
+    return m.G
 
